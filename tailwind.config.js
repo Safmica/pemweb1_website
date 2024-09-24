66010748c64c2f6bcbb66f7e86fa419eee0ddc2d
Safmica <safmikasa@gmail.com>
2024-09-24 21:51:21 +0700

    feat: add config font and color, with testing navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./html/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        harryPotter: ['harryPotter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'], 
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+      colors: {
+        customRed: '#ae0001',
+        customBlue: '#222f5b',
+        customGreen: '#2a623d',
+        customYellow: '#f0c75e',
+        customGrey: '#bebebe',
+      },
+    },
   },
   plugins: [],
-}
+};
